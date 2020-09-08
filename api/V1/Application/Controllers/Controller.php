8508d0864bfef4b142bd79cf270107ec8be43385
Luiz Lahr <boivl@hotmail.com>
2020-09-08 18:15:47 +0200

    partial - orders

--- api/V1/Application/Controllers/Controller.php
+++ api/V1/Application/Controllers/Controller.php
@@ -3,13 +3,14 @@
 namespace Borto\Application\Controllers;
 
 use Borto\Application\Traits\ApiResponse;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequestsl;
+    use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
     use ApiResponse;
